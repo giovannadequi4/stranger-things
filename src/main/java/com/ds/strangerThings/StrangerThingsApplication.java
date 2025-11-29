46a12b44840fb5d724b4c9682ee55302d11c6b1d
Giovanna <giovanna.dequi@gmail.com>
2025-11-29 12:55:48 -0300

    commit inicial

---
+++ src/main/java/com/ds/strangerThings/StrangerThingsApplication.java
@@ -0,0 +1,13 @@
+package com.ds.strangerThings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrangerThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrangerThingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/strangerThings/StrangerThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.strangerThings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrangerThingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
